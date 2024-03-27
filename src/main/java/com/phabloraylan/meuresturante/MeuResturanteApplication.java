1fc3e6685ebe4dc868330973fb3e8689a3e01335
Phablo Raylan <phabloraylan@gmail.com>
2024-03-26 21:15:21 -0300

    init

---
+++ src/main/java/com/phabloraylan/meuresturante/MeuResturanteApplication.java
@@ -0,0 +1,13 @@
+package com.phabloraylan.meuresturante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuResturanteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuResturanteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phabloraylan/meuresturante/MeuResturanteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phabloraylan.meuresturante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuResturanteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
